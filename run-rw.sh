06da778502db60ffaa055e0950382047a09673f4
Eric Ernst <eric.ernst@intel.com>
2018-09-27 02:43:22 +0000

    add random read/write scripts as well as measurements script
    Signed-off-by: Eric Ernst <eric.ernst@intel.com>

---
+++ run-rw.sh
@@ -0,0 +1,28 @@
+#!/bin/bash 
+
+
+iodepths=(1 2 4 8 16 32 1024)
+
+for c in "${iodepths[@]}"; do
+
+  jobs=$c
+  if [ $c -gt "8" ]
+  then 
+	jobs=8
+  fi
+
+  echo iterating: queue $c, numjobs $jobs
+  
+  sudo fio  --size=8G --direct=1 --ioengine=libaio \
+	--filename=/dev/nvme0n1 --overwrite=1 --readwrite=randwrite \
+       	--bs=4K --runtime=180 --time_based --ramp_time=60 --invalidate=1 --clocksource=clock_gettime \
+	--iodepth=$c --numjobs=$jobs  --name=iteration_$c &> rw-q-$c-results.txt &
+  fiopid=$!
+
+  echo goign to sleep for a bit
+  sleep 60
+  echo "yawn, let's wait for pid $ifopid to finish"
+  ./measure.sh 4 20 > rw-q-$c-measurements.log
+  wait $fiopid
+  echo "done"
+done
